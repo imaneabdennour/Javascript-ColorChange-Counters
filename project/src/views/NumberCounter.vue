76a9667d26646b965d269dae6b171271e0fa1fc3
Imane Abdennour <imane.abdennour0@gmail.com>
2020-09-02 00:54:50 +0100

    final

--- project/src/views/NumberCounter.vue
+++ project/src/views/NumberCounter.vue
@@ -3,5 +3,45 @@
     <br />
     <h1>NUMBER COUNTER</h1>
     <br /><br />
+    <div class="carre">
+      <h2>{{ count }}</h2>
+      <br />
+      <button @click="Decrem" class="btnMoins">-</button>
+      <button @click="Increm" class="btnPlus">+</button>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    Increm() {
+      this.count++;
+    },
+    Decrem() {
+      this.count--;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+h2 {
+  margin-top: 20px;
+}
+.carre {
+  margin: auto;
+  width: 200px;
+  height: 200px;
+  border: 2px solid brown;
+}
+.btnMoins {
+  margin-right: 100px;
+  margin-top: 50px;
+}
+</style>
